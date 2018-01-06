66f1b8658b4c2ff9b88da80df72184f943ae2bb3
Alan Richey <alan@icant.dance>
2018-01-06 12:06:11 -0600

    Initial commit. 15 minutes elapsed.

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <elevator></elevator>
+  </div>
+</template>
+
+<script>
+import elevator from './components/elevator'
+
+export default {
+  name: 'app',
+  components: {
+    elevator
+  },
+  data () {
+    return {
+      bottomFloor: 1,
+      floors: 1,
+      isRequested: false,
+      elevators: [
+        { isMoving }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/elevator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="elevator">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'elevator',
+  data () {
+    return {
+      floors: 1,
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
